92505bbb63fe69b7765cbf2283a1803ed8dc82c5
Shreyas S S <shreyasnarve1611@gmail.com>
2024-07-17 13:25:59 +0530

    add postgresql db and create entity class

---
+++ src/main/java/orbitor/bionic/cachedapi/controller/AlbumController.java
@@ -0,0 +1,13 @@
+package orbitor.bionic.cachedapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AlbumController {
+
+//    @GetMapping("/albums")
+//    public
+}
---
+++ src/main/java/orbitor/bionic/cachedapi/entity/Album.java
@@ -0,0 +1,35 @@
+package orbitor.bionic.cachedapi.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "albums")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "artist")
+    private String artist;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "total_tracks")
+    private Integer totalTracks;
+
+    @Column(name = "duration_minutes")
+    private Integer durationMinutes;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
